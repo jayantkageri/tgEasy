92623c0d86980cc81dc3c3504cc653336a819fb0
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-14 19:46:56 +0530

    Method has no argument

--- tgEasy/__init__.py
+++ tgEasy/__init__.py
@@ -118,7 +118,7 @@ class tgClient(Methods, Scaffold):
         self.__client__.stop()
         logging.info("Stopped the pyrogram.Client")
 
-    def runClients(*args):
+    def runClients(self, *args):
         """
     ### `tgEast.tgClint.runClients()`
     - Runs the Multiple `pyrogram.Client` of tgEasy by adding `tgEasy.tgClient.run()` in your main file and run.
